798e51705966e278a7156da0acb79d23010424c5
rportugal-ux <r.portugal@ba.estudante.senai.br>
2026-05-25 17:24:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("formContato");
+
+form.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    alert("Mensagem enviada com sucesso!");
+
+});
